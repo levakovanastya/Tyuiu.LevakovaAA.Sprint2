7f5403dcbad61113a245a12fa53294052aa7a82f
Настя Левакова <tyuiu.levakovaaa@outlook.com>
2025-10-17 06:57:17 +0500

    Task4

--- Tyuiu.LevakovaAA.Sprint2.Task4.V28.Test/DataServiceTest.cs
+++ Tyuiu.LevakovaAA.Sprint2.Task4.V28.Test/DataServiceTest.cs
@@ -21,10 +21,10 @@ namespace Tyuiu.LevakovaAA.Sprint2.Task4.V28.Test
         public void ValidCondition2()
         {
             DataService ds = new DataService();
-            double x = 1;
+            double x = 5;
             double y = 5;
             double res = ds.Calculate(x, y);
-            double wait = 0.391;
+            double wait = 1.787;
             Assert.AreEqual(wait, res);
         }
     }
